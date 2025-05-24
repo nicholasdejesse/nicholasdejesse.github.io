dc7939e3f6d9983129bfc4f384787c9a784ce4e0
NicoBobBison <47168714+NicoBobBison@users.noreply.github.com>
2025-05-24 00:06:24 -0700

    Navbar fixed, down chevron

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,16 @@ import "./App.css"
 function App(){
   return <>
   <div className="hero">
-    <h1>Nicholas DeJesse</h1>
-    <h2>Software Engineer, Web Developer, Researcher</h2>
+    <nav>
+      <ul>
+        <li><img className="github" src="src\assets\github-mark-white.svg" alt="GitHub link"/></li>
+      </ul>
+    </nav>
+    <div className="titles">
+      <h1>Nicholas DeJesse</h1>
+      <h2>Software Engineer, Web Developer, Researcher</h2>
+    </div>
+    <img className="down_chevron" src="src\assets\chevron_down_solid.svg" alt="Down arrow"/>
   </div>
   </>
 }
