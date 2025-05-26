6d2e6c9472c3de6cd998dfbd406a4904c147fbf3
NicoBobBison <47168714+NicoBobBison@users.noreply.github.com>
2025-05-25 22:27:37 -0700

    Removed chevron, test description

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
 import "./App.css"
+import "./components/Description"
+import Description from "./components/Description"
 
-function App(){
+function TestDescription(){
+  const description = ["Did stuff", "Wrote things", "Won award"]
+  return <Description title="Test" dates="Jan 2024 - Present" subtitle="Researcher" description={description}/>
+}
+
+export default function App(){
   return <>
+  <nav>
+    <ul>
+      <li><a href="https://github.com/NicoBobBison" target="_blank"><img className="github" src="src\assets\github-mark-white.svg" alt="GitHub link"/></a></li>
+    </ul>
+  </nav>
   <div className="hero">
-    <nav>
-      <ul>
-        <li><img className="github" src="src\assets\github-mark-white.svg" alt="GitHub link"/></li>
-      </ul>
-    </nav>
     <div className="titles">
       <h1>Nicholas DeJesse</h1>
       <h2>Software Engineer, Web Developer, Researcher</h2>
     </div>
-    <img className="down_chevron" src="src\assets\chevron_down_solid.svg" alt="Down arrow"/>
+  </div>
+  <div className="content">
+    <div className="descriptions">
+      <TestDescription />
+    </div>
   </div>
   </>
 }
-
-export default App
---
+++ src/components/Description.tsx
@@ -0,0 +1,26 @@
+import "./Description.css"
+
+interface DescriptionProps {
+    title: string;
+    dates: string;
+    subtitle: string;
+    description: string[];
+}
+
+export default function Description(props: DescriptionProps){
+    const title = props.title;
+    const dates = props.dates;
+    const subtitle = props.subtitle;
+    const description = props.description;
+
+    return <div className="container">
+        <div className="title-dates">
+            <h1 className="title">{title}</h1>
+            <h2 className="dates">{dates}</h2>
+        </div>
+        <h3 className="subtitle">{subtitle}</h3>
+        <ul>
+            {description.map((d) => <li>{d}</li>)}
+        </ul>
+    </div>
+}
